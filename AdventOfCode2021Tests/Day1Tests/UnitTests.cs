c695f4650277a5a66d90e1d99dbbf47f39c696d1
Thomas Twigden <ttwigden@deloitte.co.uk>
2021-12-06 21:58:00 +0000

    Added additional test for no numbers in input list.

--- AdventOfCode2021Tests/Day1Tests/UnitTests.cs
+++ AdventOfCode2021Tests/Day1Tests/UnitTests.cs
@@ -7,28 +7,45 @@ namespace AdventOfCodeTests
     [TestClass]
     public class UnitTests
     {
-        [TestMethod]
-        public void Return_0_For_A_Single_Number_Input()
+        [TestMethod, TestCategory("No inputs")]
+        public void Return_0_Empty_List()
+        {
+            List<int> depthReadings = new List<int> { };
+            var depthReport = SeaFloorDepth.rateOfIncrease(depthReadings);
+            Assert.AreEqual(0, depthReport);
+        }
+
+        [TestMethod, TestCategory("Single number input")]
+        public void Return_0()
         {
             List<int> depthReadings = new List<int> { 0 };
             var depthReport = SeaFloorDepth.rateOfIncrease(depthReadings);
             Assert.AreEqual(0, depthReport);
         }
 
-        [TestMethod]
-        public void Return_0_For_A_2_Number_Input_Where_The_Second_Number_Is_Less_Than_The_First()
+        [TestMethod, TestCategory("Two number input")]
+        public void Return_0_Where_The_Second_Number_Is_Less_Than_The_First()
         {
             List<int> depthReadings = new List<int> { 100, 50 };
             var depthReport = SeaFloorDepth.rateOfIncrease(depthReadings);
             Assert.AreEqual(0, depthReport);
         }
 
-        [TestMethod]
-        public void Return_1_For_A_2_Number_Input_Where_The_Second_Number_Is_Greater_Than_The_First()
+        [TestMethod, TestCategory("Two number input")]
+        public void Return_0_Where_The_Second_Number_Is_The_Same_As_The_First()
+        {
+            List<int> depthReadings = new List<int> { 50, 50 };
+            var depthReport = SeaFloorDepth.rateOfIncrease(depthReadings);
+            Assert.AreEqual(0, depthReport);
+        }
+
+        [TestMethod, TestCategory("Two number input")]
+        public void Return_1_Where_The_Second_Number_Is_Greater_Than_The_First()
         {
             List<int> depthReadings = new List<int> { 50, 100 };
             var depthReport = SeaFloorDepth.rateOfIncrease(depthReadings);
             Assert.AreEqual(1, depthReport);
         }
+
     }
 }
