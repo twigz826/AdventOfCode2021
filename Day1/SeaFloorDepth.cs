c3454d95ec96877a8a128852ccb7062e71ba3d22
Thomas Twigden <ttwigden@deloitte.co.uk>
2021-12-06 21:26:58 +0000

    Add project files.

---
+++ Day1/SeaFloorDepth.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021.Day1
+{
+    public static class SeaFloorDepth
+    {
+        public static int rateOfIncrease(List<int> depthReadings)
+        {
+            return 0;
+        }
+    }
+}
